7f55c8ac4574197032a8749fcc1af40d5687cae3
Âu Thiên Phước <126746510+thienphuoc261@users.noreply.github.com>
2023-12-06 08:59:22 +0700

    Comitted 6/12/2023

---
+++ Data/Carts.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ptm_store_service.Data
+{
+    [Table("CartLines")]
+    public class Carts
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int? UserID { get; set; }
+        [ForeignKey("UserId")]
+        public Users User { get; set; }
+
+        public virtual ICollection<CartLines> CartLines { get; set; }
+    }
+}
